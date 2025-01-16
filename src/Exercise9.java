e0eb173e3a217ff6199ab6e72644041cd04d40bb
Ubeimar Vargas <uavargas23@gmail.com>
2025-01-16 10:13:31 -0500

    Ejercicio 9 realizado.

--- src/Exercise9.java
+++ src/Exercise9.java
@@ -14,11 +14,20 @@ public class Exercise9 {
     public static void main(String[] args) {
         // instanciamos un objeto de la clase Scanner
         Scanner input = new Scanner(System.in);
-        double datoGrados;
+        double gradosCelsius;
         System.out.print("Ingrese el valor en grados Celsius: ");
-        datoGrados = input.nextDouble();
+        gradosCelsius = input.nextDouble();
 
         //Proceso de las operaciones de conversion de la temperatura
+        double fahrenheit = gradosCelsius * 9/5 + 32;
+        double kelvin     = gradosCelsius + 273.15;
+
+        // Mostramos los resultados de las conversiones
+
+        System.out.printf("%.1f grados Celsius equivalen a %.1f grados Fahrenheit y a %.1f grados kelvin",
+                gradosCelsius,fahrenheit,kelvin);
+
+
 
 
     }
